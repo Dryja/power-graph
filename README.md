c369c251f3dc687e3c05b6f800c89beacdd6ec0a
Hubert Dryja <hubert.dryja@lingarogroup.com>
2020-04-11 23:48:58 +0200

    readme + docker for easier running, added build frontend for easier boot up - know it is ugly sorry

---
+++ README.md
@@ -0,0 +1,16 @@
+# Power Graph
+
+## How to run
+
+The simplest way to start the app is to start it from docker-compose by typing:
+
+```
+docker-compose up
+```
+
+After containers boot up you can access the app at http://127.0.0.1:8000/
+
+API docs available at http://127.0.0.1:3000/docs
+
+
+![screenshot](screen.png)
---
+++ frontend/build/precache-manifest.a488e0cc9867f90b904878e20ff3bc01.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8febccd5cfdf761a323944e87f720034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04304c8f38ca13e5dd6c",
+    "url": "/static/css/2.254b507e.chunk.css"
+  },
+  {
+    "revision": "dccfc32609c786fca2bc",
+    "url": "/static/css/main.6a792cc9.chunk.css"
+  },
+  {
+    "revision": "04304c8f38ca13e5dd6c",
+    "url": "/static/js/2.5a0d2b26.chunk.js"
+  },
+  {
+    "revision": "1196017f5f7164485323e2f8bab19f54",
+    "url": "/static/js/2.5a0d2b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dccfc32609c786fca2bc",
+    "url": "/static/js/main.50f9bbd8.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "a1a2d01bcd034270a3bc92176edfb66c",
+    "url": "/static/media/addNodeIcon.a1a2d01b.png"
+  },
+  {
+    "revision": "dd0baa69a69937600dc3bf035f3873c4",
+    "url": "/static/media/backIcon.dd0baa69.png"
+  },
+  {
+    "revision": "d5267b8db2498e44bd567bd12e07576b",
+    "url": "/static/media/connectIcon.d5267b8d.png"
+  },
+  {
+    "revision": "260c9c6535065674bb5e59a73f894abb",
+    "url": "/static/media/cross.260c9c65.png"
+  },
+  {
+    "revision": "02d321edc6e03aea1675001899d8f479",
+    "url": "/static/media/deleteIcon.02d321ed.png"
+  },
+  {
+    "revision": "d5422321c4f6feed4081891051f9a6b2",
+    "url": "/static/media/editIcon.d5422321.png"
+  }
+]);
